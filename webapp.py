906374486dc7ae07c20283ea898c0efcf1cfec54
Ramon-W <77691280+Ramon-W@users.noreply.github.com>
2021-11-07 15:06:23 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -17,12 +17,15 @@ def render_main():
   with open('windturbine.json') as d:
     data = json.load(d)
   grid_code = ''
+  grid_code_basic = ''
   row = 0    
   square = 0
   while row < 15:
     grid_code += '<tr>'
+    grid_code_basic += '<tr>'
     while square < 24:
       grid_code += '<td>'
+      grid_code_basic += '<td>'
       point = chr(row + 65) + str(square + 1)
       if point in data:
         green = (((1 / float((data[point]['200wind'] + data[point]['150wind'] + data[point]['100wind'])/3) - 0.125) * 165) / (0.1667 - 0.125))
@@ -31,12 +34,19 @@ def render_main():
                       str(data[point]['100wind']) + '<br>Wind Speed 150m (m/s): ' + str(data[point]['150wind']) + '<br>Wind Speed 200m (m/s): ' + 
                       str(data[point]['200wind']) + '<br>Depth: ' + str(data[point]['depth']) +
                       '" data-toggle="popover" data-trigger="focus" data-content="popover"></button>')
+        grid_code_basic += ('<button class="square-button" data-html="true" data-placement="auto right" title="' +
+                            data[point]['coordinates'] + '" data-toggle="popover" data-trigger="focus" data-content="Wind Speed 100m (m/s): ' +
+                            str(data[point]['100wind']) + '<br>Wind Speed 150m (m/s): ' + str(data[point]['150wind']) + '<br>Wind Speed 200m (m/s): ' + 
+                            str(data[point]['200wind']) + '<br>Depth: ' + str(data[point]['depth']) +
+                            '" data-toggle="popover" data-trigger="focus" data-content="popover"></button>')
       grid_code += '</td>'
+      grid_code_basic += '<td>'
       square += 1
     grid_code += '</tr>'
+    grid_code_basic += '</tr>'
     square = 0
     row += 1
-  return render_template('main.html', test = Markup(grid_code))
+  return render_template('main.html', wind_grid = Markup(grid_code), basic_grid = Markup(grid_code_basic))
 
 if __name__ == "__main__":
     app.run()
