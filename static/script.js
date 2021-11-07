93b02a4e8549eb0bce91fba1efd7af9c90cb8541
Ramon-W <77691280+Ramon-W@users.noreply.github.com>
2021-11-06 22:43:41 -0700

    Update script.js

--- static/script.js
+++ static/script.js
@@ -6,24 +6,24 @@ $(document).ready(function(){
     $("#basic-map").show();
     $("#bathymetry-map").hide();
     $("#marine-conservation-map").hide();
-    $("#basic-button").replaceWith("<li class='active pointer' id='basic-button'><a>Basic Map<button class='info-buttons' data-toggle='tooltip' data-placement='right' title='Basic Information!'><span class='glyphicon glyphicon-info-sign'></span></button></a></li>");
-    $("#bathymetry-button").replaceWith("<li class='pointer' id='bathymetry-button'><a>Bathymetry Map<button class='info-buttons' data-toggle='tooltip' data-placement='right' title='Depth Information!'><span class='glyphicon glyphicon-info-sign'></span></button></a></li>");
-    $("#marine-conservation-button").replaceWith("<li class='pointer' id='marine-conservation-button'><a>Marine Conservation Map<button class='info-buttons' data-toggle='tooltip' data-placement='right' title='Marine Conservation Information!'><span class='glyphicon glyphicon-info-sign'></span></button></a></li>");
+    $("#basic-button").addClass("active");
+    $("#bathymetry-button").removeClass("active");
+    $("#marine-conservation-button").removeClass("active");
   });
   $("#bathymetry-button").click(function() {
     $("#basic-map").hide();
     $("#bathymetry-map").show();
     $("#marine-conservation-map").hide();
-    $("#basic-button").replaceWith("<li class='pointer' id='basic-button'><a>Basic Map<button class='info-buttons' data-toggle='tooltip' data-placement='right' title='Basic Information!'><span class='glyphicon glyphicon-info-sign'></span></button></a></li>");
-    $("#bathymetry-button").replaceWith("<li class='active pointer' id='bathymetry-button'><a>Bathymetry Map<button class='info-buttons' data-toggle='tooltip' data-placement='right' title='Depth Information!'><span class='glyphicon glyphicon-info-sign'></span></button></a></li>");
-    $("#marine-conservation-button").replaceWith("<li class='pointer' id='marine-conservation-button'><a>Marine Conservation Map<button class='info-buttons' data-toggle='tooltip' data-placement='right' title='Marine Conservation Information!'><span class='glyphicon glyphicon-info-sign'></span></button></a></li>");
+    $("#basic-button").removeClass("active");
+    $("#bathymetry-button").addClass("active");
+    $("#marine-conservation-button").removeClass("active");
   });
   $("#marine-conservation-button").click(function() {
     $("#basic-map").hide();
     $("#bathymetry-map").hide();
     $("#marine-conservation-map").show();
-    $("#basic-button").replaceWith("<li class='pointer' id='basic-button'><a>Basic Map<button class='info-buttons' data-toggle='tooltip' data-placement='right' title='Basic Information!'><span class='glyphicon glyphicon-info-sign'></span></button></a></li>");
-    $("#bathymetry-button").replaceWith("<li class='pointer' id='bathymetry-button'><a>Bathymetry Map<button class='info-buttons' data-toggle='tooltip' data-placement='right' title='Depth Information!'><span class='glyphicon glyphicon-info-sign'></span></button></a></li>");
-    $("#marine-conservation-button").replaceWith("<li class='active pointer' id='marine-conservation-button'><a>Marine Conservation Map<button class='info-buttons' data-toggle='tooltip' data-placement='right' title='Marine Conservation Information!'><span class='glyphicon glyphicon-info-sign'></span></button></a></li>");
+    $("#basic-button").removeClass("active");
+    $("#bathymetry-button").removeClass("active");
+    $("#marine-conservation-button").addClass("active");
   });
 });
