4c0fc57199e5751191d578cbdb5b411036eedeb7
Ramon-W <77691280+Ramon-W@users.noreply.github.com>
2021-11-07 13:30:20 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -25,7 +25,8 @@ def render_main():
       grid_code += '<td>'
       point = chr(row + 65) + str(square + 1)
       if point in data:
-        grid_code += ('<button class="square-button" data-html="true" data-placement="auto right" title="' +
+        red = data[point]['200wind'] * 25
+        grid_code += ('<button style="color:rgba(red, 76, 76, 0.3);" class="square-button" data-html="true" data-placement="auto right" title="' +
                       data[point]['coordinates'] + '" data-toggle="popover" data-trigger="focus" data-content="Wind Speed 100m (m/s): ' +
                       str(data[point]['100wind']) + '<br>Wind Speed 150m (m/s): ' + str(data[point]['150wind']) + '<br>Wind Speed 200m (m/s): ' + 
                       str(data[point]['200wind']) + '<br>Depth: ' + str(data[point]['depth']) +
