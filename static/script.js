0def61ba197ea9698b0de043c4bb64aa7a0d9b96
Ramon-W <77691280+Ramon-W@users.noreply.github.com>
2021-11-06 21:21:24 -0700

    Update script.js

--- static/script.js
+++ static/script.js
@@ -1,3 +1,20 @@
 $(document).ready(function(){
+  $("#marine-conservation-map").hide();
+  $("#bathymetry-map").hide();
   $('[data-toggle="tooltip"]').tooltip();
+  $( "#basic-button").click(function() {
+    $("#basic-map").show();
+    $("#bathymetry-map").hide();
+    $("#marine-conservation-map").hide();
+  });
+  $( "#bathymetry-button").click(function() {
+    $("#basic-map").hide();
+    $("#bathymetry-map").show();
+    $("#marine-conservation-map").hide();
+  });
+  $( "#marine-conservation-button").click(function() {
+    $("#basic-map").hide();
+    $("#bathymetry-map").hide();
+    $("#marine-conservation-map").show();
+  });
 });
