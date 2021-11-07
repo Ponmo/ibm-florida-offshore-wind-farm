17eaa273d3b669dabefdda519b7be36a70a4fa3f
Ramon-W <77691280+Ramon-W@users.noreply.github.com>
2021-11-07 14:23:56 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -25,14 +25,14 @@ def render_main():
       grid_code += '<td>'
       point = chr(row + 65) + str(square + 1)
       if point in data:
-        green = (((1 / float(data[point]['200wind']) - 0.125) * 255) / (0.1667 - 0.125))
-        grid_code += ('<button style="background-color:rgba(255,' + str(green) + ', 0, 0.25);" class="square-button" data-html="true" data-placement="auto right" title="' +
+        green = (((1 / float(data[point]['200wind']) - 0.125) * 165) / (0.1667 - 0.125))
+        grid_code += ('<button style="background-color:rgba(255,' + str(green) + ', 0, 0.75);" class="square-button" data-html="true" data-placement="auto right" title="' +
                       data[point]['coordinates'] + '" data-toggle="popover" data-trigger="focus" data-content="Wind Speed 100m (m/s): ' +
                       str(data[point]['100wind']) + '<br>Wind Speed 150m (m/s): ' + str(data[point]['150wind']) + '<br>Wind Speed 200m (m/s): ' + 
                       str(data[point]['200wind']) + '<br>Depth: ' + str(data[point]['depth']) +
                       '" data-toggle="popover" data-trigger="focus" data-content="popover"></button>')
       else:
-        grid_code += '<div style="background-color:rgba(200, 200, 200, 0.25);" class="square-button-empty"></div>'
+        grid_code += '<div style="background-color:rgba(200, 200, 200, 0.15);" class="square-button-empty"></div>'
       grid_code += '</td>'
       square += 1
     grid_code += '</tr>'
