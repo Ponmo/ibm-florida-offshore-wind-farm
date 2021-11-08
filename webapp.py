09019ac519ca15893973ce5acc2a49f3d71d4342
Ramon-W <77691280+Ramon-W@users.noreply.github.com>
2021-11-07 16:26:32 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -28,17 +28,24 @@ def render_main():
       grid_code_basic += '<td>'
       point = chr(row + 65) + str(square + 1)
       if point in data:
+        value = ''
+        if row == 0 and square == 0:
+          value = 'A1'
+        elif row == 0:
+          value = str(square + 1)
+        elif square == 0:
+          value = chr(row + 65)
         green = (((1 / float((data[point]['200wind'] + data[point]['150wind'] + data[point]['100wind'])/3) - 0.125) * 165) / (0.1667 - 0.125))
         grid_code += ('<button style="background-color:rgba(255,' + str(green) + ', 0, 0.75);" class="square-button" data-html="true" data-placement="auto right" title="' +
                       data[point]['coordinates'] + '" data-toggle="popover" data-trigger="focus" data-content="Wind Speed 100m (m/s): ' +
                       str(data[point]['100wind']) + '<br>Wind Speed 150m (m/s): ' + str(data[point]['150wind']) + '<br>Wind Speed 200m (m/s): ' + 
                       str(data[point]['200wind']) + '<br>Depth (m): ' + str(data[point]['depth']) +
-                      '" data-toggle="popover" data-trigger="focus" data-content="popover"></button></td>')
+                      '" data-toggle="popover" data-trigger="focus" data-content="popover">' + value + '</button></td>')
         grid_code_basic += ('<button class="square-button" data-html="true" data-placement="auto right" title="' +
                             data[point]['coordinates'] + '" data-toggle="popover" data-trigger="focus" data-content="Wind Speed 100m (m/s): ' +
                             str(data[point]['100wind']) + '<br>Wind Speed 150m (m/s): ' + str(data[point]['150wind']) + '<br>Wind Speed 200m (m/s): ' + 
                             str(data[point]['200wind']) + '<br>Depth (m): ' + str(data[point]['depth']) +
-                            '" data-toggle="popover" data-trigger="focus" data-content="popover"></button></td>')
+                            '" data-toggle="popover" data-trigger="focus" data-content="popover">' + value + '</button></td>')
       else:
         value = ''
         if row == 0 and square == 0:
