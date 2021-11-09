d6a6efd3bfc3e7d6faff8cd4ee782f804b6c9587
Ramon-W <77691280+Ramon-W@users.noreply.github.com>
2021-11-08 19:17:34 -0800

    Update script.js

--- static/script.js
+++ static/script.js
@@ -38,9 +38,9 @@ $(document).ready(function(){
     const str = this.attributes[7].value;
     const indexOne = [...str.matchAll(new RegExp('</b>', 'gi'))].map(a => a.index);
     const indexTwo = [...str.matchAll(new RegExp('<br>', 'gi'))].map(a => a.index);
-    const maxWind = str.substring(indexOne[0] + 4, indexTwo[0]);
-    const medWind = str.substring(indexOne[1] + 4, indexTwo[1]);
-    const lowWind = str.substring(indexOne[2] + 4, indexTwo[2];
+    const maxWind = str.substring(parseInt(indexOne[0]) + 4, indexTwo[0]);
+    const medWind = str.substring(parseInt(indexOne[1]) + 4, indexTwo[1]);
+    const lowWind = str.substring(parseInt(indexOne[2]) + 4, indexTwo[2];
     alert(maxWind);
   });
 });
