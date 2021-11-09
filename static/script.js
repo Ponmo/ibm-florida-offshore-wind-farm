effb92dfcdca60a3cd11820f9103f22b91c3e1e2
Ramon-W <77691280+Ramon-W@users.noreply.github.com>
2021-11-09 11:08:09 -0800

    Update script.js

--- static/script.js
+++ static/script.js
@@ -39,22 +39,26 @@ $(document).ready(function(){
     const str = this.attributes[7].value;
     const indexOne = [...str.matchAll(new RegExp('</b>', 'gi'))].map(a => a.index);
     const indexTwo = [...str.matchAll(new RegExp('<br>', 'gi'))].map(a => a.index);
-    if(hubHeight == "100") {
-      velocity = str.substring(indexOne[2] + 4, indexTwo[2]);
-    }
-    else if(hubHeight == "150") {
-      velocity = str.substring(indexOne[1] + 4, indexTwo[1]);
-    }
-    else {
-      velocity = str.substring(indexOne[0] + 4, indexTwo[0]);
-    }
+    velocityMin = str.substring(indexOne[2] + 4, indexTwo[2]);
+    velocityMed = str.substring(indexOne[1] + 4, indexTwo[1]);
+    velocityMax = str.substring(indexOne[0] + 4, indexTwo[0]);
     evaluateData();
   });
-  
+    
   function evaluateData() {
     var hubHeight = parseFloat(document.getElementById('height-selection').value);
     if(isNaN(hubHeight)) {
       hubHeight = 100.0;
+    }
+    var velocity = 0;
+    if(hubHeight == "100") {
+      velocity = velocityMin;
+    }
+    else if(hubHeight == "150") {
+      velocity = velocityMed;
+    }
+    else {
+      velocity = velocityMax;
     }
     var radius = parseFloat(document.getElementById('radius-selection').value);
     if(isNaN(radius)) {
