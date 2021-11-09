77049f2f77b4eeab79be3c4bcea3268bbd193d4a
Ramon-W <77691280+Ramon-W@users.noreply.github.com>
2021-11-08 17:36:16 -0800

    Update script.js

--- static/script.js
+++ static/script.js
@@ -35,7 +35,7 @@ $(document).ready(function(){
     $("#basic-grid").show();
   });
   $(".square-button").click(function() {
-    alert(this.attributes[4].value)
+    alert(this.attributes[4].value);
     $.each(this.attributes, function() {
       // this.attributes is not a plain object, but an array
       // of attribute nodes, which contain both the name and value
