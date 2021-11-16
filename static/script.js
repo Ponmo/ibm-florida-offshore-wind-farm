23a0ac9e0f678010d123bcc278e5dc8ca05e5700
Ramon-W <77691280+Ramon-W@users.noreply.github.com>
2021-11-15 23:10:58 -0800

    Update script.js

--- static/script.js
+++ static/script.js
@@ -123,7 +123,7 @@ $(document).ready(function(){
   });
   $("path").click(function() {
     const name = $(this).attr('id').replace('-', ' ');
-    $("#county-info").html("<b>" + name + " County<b><br>" + counties[name]);
+    $("#county-info").html("<b>" + name + " County</b><br>" + counties[name]);
   });
   $(".square-button").click(function() {
     const str = this.attributes[7].value;
