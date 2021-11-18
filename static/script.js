cdd1ce2adfda6b67061b98a78b911c94cd3a678d
Ramon-W <77691280+Ramon-W@users.noreply.github.com>
2021-11-17 16:22:59 -0800

    Update script.js

--- static/script.js
+++ static/script.js
@@ -147,7 +147,7 @@ $(document).ready(function(){
     $("#county-info").html("<b style='font-size: 15px;'>" + name + " County</b><br>" + counties[name]);
   });
   $(".square-button").click(function() {
-    $("#tool-bar-header-title").html("Power Estimate — " + $(this).attr('id'));
+    $("#tool-bar-header-title-three").html("Power Estimate — " + $(this).attr('id'));
     const str = this.attributes[7].value;
     const indexOne = [...str.matchAll(new RegExp('</b>', 'gi'))].map(a => a.index);
     const indexTwo = [...str.matchAll(new RegExp('<br>', 'gi'))].map(a => a.index);
