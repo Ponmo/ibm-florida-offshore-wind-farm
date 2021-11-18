cfafa3ee0a3ec1cb45f205f5b799ffc5b6e9b9bb
Ramon-W <77691280+Ramon-W@users.noreply.github.com>
2021-11-18 11:40:15 -0800

    Update webapp.py

--- webapp.py
+++ webapp.py
@@ -36,16 +36,16 @@ def render_main():
         elif square == 0:
           value = chr(row + 65)
         green = (((1 / float((data[point]['200wind'] + data[point]['150wind'] + data[point]['100wind'])/3) - 0.125) * 165) / (0.1667 - 0.125))
-        grid_code += ('<a style="background-color:rgba(255,' + str(green) + ', 0, 0.75);" class="square-button" data-html="true" data-placement="auto right" title="<strong>' +
+        grid_code += ('<a style="background-color:rgba(255,' + str(green) + ', 0, 0.75);" class="square-button ' + chr(row + 65) + str(square + 1) + '" data-html="true" data-placement="auto right" title="<strong>' +
                       data[point]['coordinates'] + '</strong>" data-toggle="popover" data-trigger="focus" data-content="<b>Wind Speed 200m (m/s):</b> ' +
                       str(data[point]['200wind']) + '<br><b>Wind Speed 150m (m/s):</b> ' + str(data[point]['150wind']) + '<br><b>Wind Speed 100m (m/s):</b> ' + 
                       str(data[point]['100wind']) + '<br><b>Depth (m):</b> ' + str(data[point]['depth']) +
-                      '"tabindex="0" role="button" class="' + chr(row + 65) + str(square + 1) + '">' + value + '</a></td>')
-        grid_code_basic += ('<a style="background-color: transparent;" class="square-button" data-html="true" data-placement="auto right" title="<strong>' +
+                      '"tabindex="0" role="button">' + value + '</a></td>')
+        grid_code_basic += ('<a style="background-color: transparent;" class="square-button ' + chr(row + 65) + str(square + 1) + '" data-html="true" data-placement="auto right" title="<strong>' +
                             data[point]['coordinates'] + '</strong>" data-toggle="popover" data-trigger="focus" data-content="<b>Wind Speed 200m (m/s):</b> ' +
                             str(data[point]['200wind']) + '<br><b>Wind Speed 150m (m/s):</b> ' + str(data[point]['150wind']) + '<br><b>Wind Speed 100m (m/s):</b> ' + 
                             str(data[point]['100wind']) + '<br><b>Depth (m):</b> ' + str(data[point]['depth']) +
-                            '"tabindex="0" role="button" class="' + chr(row + 65) + str(square + 1) + '">' + value + '</a></td>')
+                            '"tabindex="0" role="button">' + value + '</a></td>')
       else:
         value = ''
         if row == 0 and square == 0:
